5cf832fe4bae6fb2453cb086d55c4cd232db6054
aroravaibhav <aroravaibhav@live.com>
2019-04-15 22:38:02 +0530

    Initial commit

--- MultiSliderIssue/ViewController.swift
+++ MultiSliderIssue/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import MultiSlider
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ms1: MultiSlider!
+    @IBOutlet weak var ms2: MultiSlider!
+    @IBOutlet weak var ms3: MultiSlider!
+    @IBOutlet weak var ms4: MultiSlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateSliders()
     }
 
+    func updateSliders(){
+        setSliders(slider: ms1)
+        setSliders(slider: ms2)
+        setSliders(slider: ms3)
+        setSliders(slider: ms4)
+    }
+    
+    func setSliders(slider: MultiSlider){
+        slider.orientation = .horizontal
+        slider.minimumValue = CGFloat(0)
+        slider.maximumValue = CGFloat(100)
+        slider.thumbCount = 1
+        slider.trackWidth = 2
+        slider.snapStepSize = 1
+        slider.showsThumbImageShadow = false
+        slider.layoutSubviews()
+        //        slider.valueLabelPosition = .bottom
+        //        slider.isValueLabelRelative = true
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.outerTrackColor = #colorLiteral(red: 0.2711775303, green: 0.5228122473, blue: 0.8482171893, alpha: 1)
+    }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MultiSliderIssue' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MultiSliderIssue
+
+  pod 'MultiSlider'
+
+  target 'MultiSliderIssueTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MultiSliderIssueUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
