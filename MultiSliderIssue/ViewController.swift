b7e5f12899da35023c23bd57e9ddb84e41056156
aroravaibhav <aroravaibhav@live.com>
2019-04-15 18:28:52 +0530

    Initial Commit

---
+++ MultiSliderIssue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiSliderIssue
+//
+//  Created by apple on 4/15/19.
+//  Copyright © 2019 Vaibhav Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
